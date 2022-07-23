306d97e3222de7c42106ad8a915ba8da9e190aba
arjunjanamatti <arjunjanamatti@gmail.com>
2022-07-23 19:52:24 +0530

    updated with few execptions

--- get_cov2/main.py
+++ get_cov2/main.py
@@ -1,7 +1,12 @@
-import statistics, numpy as np
+import statistics
 
 
 class gc:
+    '''
+    Exception handled:
+    - If length of series is greater than 1
+    - If mean of series is greater than 0
+    '''
     def __init__(self, data_series):
         '''
         :param data_series: list or pandas series
@@ -9,11 +14,11 @@ class gc:
         self.series_1 = data_series
 
     def cov(self):
-        if len(self.series_1) > 0:
+        if (len(self.series_1) > 1) & (statistics.mean(self.series_1) > 0):
             std_dev = statistics.stdev(self.series_1)
             std_dev = round(std_dev, 2)
             data_mean = statistics.mean(self.series_1)
             cov2 = std_dev / data_mean
-            return round(cov2,3)**2
+            return round((cov2)**2,3)
         else:
-            return "please pass a series instead of single number"
+            return "please pass a series instead of single number or mean of series needs to be greater than zero"
